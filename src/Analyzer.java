4dad954cbfdc07a9fcc0ee0b36407a35ba86882f
Wyvern <84735487+Wvvern@users.noreply.github.com>
2023-11-02 12:19:32 -0700

    Initial Commit

---
+++ src/Analyzer.java
@@ -0,0 +1,24 @@
+//=============================================================================================================================================================
+//
+// IMPLEMENT THIS CLASS HOWEVER YOU WANT, BUT DO NOT CHANGE THE SIGNATURES OF Analyzer(), addEntry(), or printResults(). YOU MUST USE addEntry() TO GET THE
+// DATA; DO NOT ACCESS OTHER CLASSES IN THE PROJECT (UNLESS THEY ARE YOURS).
+//
+public class Analyzer
+{
+   // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+   public Analyzer()
+   {
+   }
+
+   // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+   public void addEntry(final long step, final double time, final String id, final double x, final double y)
+   {
+      // System.out.println("<log>: "+ step + "," + time + "," + id + "," + x + "," + y);
+   }
+
+   // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+   public void printResults()
+   {
+      System.out.println("<your metrics here for all bodies>");
+   }
+}
