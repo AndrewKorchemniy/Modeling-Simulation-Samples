aa05288dbdb2554e3bcaf38d3b67de4b5b3e7104
Wyvern <84735487+Wvvern@users.noreply.github.com>
2023-11-06 23:01:30 -0800

    Complete Task 3

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -1,24 +1,121 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 //=============================================================================================================================================================
 //
 // IMPLEMENT THIS CLASS HOWEVER YOU WANT, BUT DO NOT CHANGE THE SIGNATURES OF Analyzer(), addEntry(), or printResults(). YOU MUST USE addEntry() TO GET THE
 // DATA; DO NOT ACCESS OTHER CLASSES IN THE PROJECT (UNLESS THEY ARE YOURS).
 //
-public class Analyzer
-{
-   // ---------------------------------------------------------------------------------------------------------------------------------------------------------
-   public Analyzer()
-   {
-   }
+public class Analyzer {
+    static class BodyAnalytics {
+        public double totalDistance;
+        public double totalTime;
+        public double minVelocity = Double.MAX_VALUE;
+        public double maxVelocity = Double.MIN_VALUE;
+        public double minAcceleration = Double.MAX_VALUE;
+        public double maxAcceleration = Double.MIN_VALUE;
+        private double totalVelocity;
+        private double totalAcceleration;
+        private double prevX = Double.NaN;
+        private double prevY = Double.NaN;
+        private double prevTime = Double.NaN;
+        private double prevVelocity = Double.NaN;
+        private final List<Double> velocities = new ArrayList<>();
+        private final List<Double> accelerations = new ArrayList<>();
 
-   // ---------------------------------------------------------------------------------------------------------------------------------------------------------
-   public void addEntry(final long step, final double time, final String id, final double x, final double y)
-   {
-      // System.out.println("<log>: "+ step + "," + time + "," + id + "," + x + "," + y);
-   }
+        public void addEntry(final double x, final double y, final double time) {
+            totalTime = time;
+            if (!Double.isNaN(prevX) && !Double.isNaN(prevY) && !Double.isNaN(prevTime)) {
+                double timeChange = time - prevTime;
+                double distance = Math.sqrt(Math.pow(x - prevX, 2) + Math.pow(y - prevY, 2));
+                totalDistance += distance;
 
-   // ---------------------------------------------------------------------------------------------------------------------------------------------------------
-   public void printResults()
-   {
-      System.out.println("<your metrics here for all bodies>");
-   }
+                double velocity = distance / timeChange;
+                if (velocity < minVelocity) minVelocity = velocity;
+                if (velocity > maxVelocity) maxVelocity = velocity;
+                totalVelocity += velocity;
+                velocities.add(velocity);
+
+                if (!Double.isNaN(prevVelocity)) {
+                    double acceleration = (velocity - prevVelocity) / timeChange;
+                    if (acceleration < minAcceleration) minAcceleration = acceleration;
+                    if (acceleration > maxAcceleration) maxAcceleration = acceleration;
+                    totalAcceleration += acceleration;
+                    accelerations.add(acceleration);
+                }
+                prevVelocity = velocity;
+            }
+            prevX = x;
+            prevY = y;
+            prevTime = time;
+        }
+
+        public double getAvgVelocity() {
+            if (velocities.size() == 0) return 0;
+            return totalVelocity / velocities.size();
+        }
+
+        public double getAvgAcceleration() {
+            if (accelerations.size() == 0) return 0;
+            return totalAcceleration / accelerations.size();
+        }
+
+        public double getStdVelocity() {
+            if (velocities.size() == 0) return 0;
+            double avg = getAvgVelocity();
+            double sum = 0;
+            for (double velocity : velocities) {
+                sum += Math.pow(velocity - avg, 2);
+            }
+            return Math.sqrt(sum / velocities.size());
+        }
+
+        public double getStdAcceleration() {
+            if (accelerations.size() == 0) return 0;
+            double avg = getAvgAcceleration();
+            double sum = 0;
+            for (double acceleration : accelerations) {
+                sum += Math.pow(acceleration - avg, 2);
+            }
+            return Math.sqrt(sum / accelerations.size());
+        }
+    }
+
+    private final Map<String, BodyAnalytics> bodies = new HashMap<>();
+
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+    public void addEntry(final long step, final double time, final String id, final double x, final double y) {
+        BodyAnalytics body = bodies.get(id);
+        if (body == null) {
+            body = new BodyAnalytics();
+            bodies.put(id, body);
+        }
+        body.addEntry(x, y, time);
+         System.out.println("<log>: " + step + "," + time + "," + id + "," + x + "," + y);
+        // Use the println statement above to slow the program down if you want to see the output in real time.
+    }
+
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+    public void printResults() {
+        for (Map.Entry<String, BodyAnalytics> entry : bodies.entrySet()) {
+            String id = entry.getKey();
+            BodyAnalytics body = entry.getValue();
+            System.out.println("Body: " + id);
+            System.out.println("Time: " + Math.round(body.totalTime));
+            System.out.println("Distance: " + Math.round(body.totalDistance * 100.0) / 100.0);
+            System.out.println("Velocity");
+            System.out.println("  min: " + body.minVelocity);
+            System.out.println("  max: " + body.maxVelocity);
+            System.out.println("  avg: " + body.getAvgVelocity());
+            System.out.println("  std: " + body.getStdVelocity());
+            System.out.println("Acceleration");
+            System.out.println("  min: " + body.minAcceleration);
+            System.out.println("  max: " + body.maxAcceleration);
+            System.out.println("  avg: " + body.getAvgAcceleration());
+            System.out.println("  std: " + body.getStdAcceleration());
+            System.out.println();
+        }
+    }
 }
--- src/Task3.java
+++ src/Task3.java
@@ -4,205 +4,152 @@ import javax.swing.SwingUtilities;
 //
 // DO NOT CHANGE EXISTING CODE IN THIS CLASS EXCEPT FOR main() AND Task3(). YOU MAY ADD YOUR OWN CODE.
 //
-public class Task3
-{
-   // ---------------------------------------------------------------------------------------------------------------------------------------------------------
-   // SELECT YOUR TEST AND ADJUST THE VIEWER HERE
-   public static void main(final String[] arguments)
-   {
-      String testDesignator = "7"; // get these from the task description
+public class Task3 {
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+    // SELECT YOUR TEST AND ADJUST THE VIEWER HERE
+    public static void main(final String[] arguments) {
+        String testDesignator = "6.A"; // get these from the task description
 
-      new Task3(testDesignator);
-   }
+        new Task3(testDesignator);
+    }
 
-   private Viewer _viewer;
+    private Viewer _viewer;
 
-   // ---------------------------------------------------------------------------------------------------------------------------------------------------------
-   // CONFIGURE YOUR TESTS HERE
-   public Task3(final String testDesignator)
-   {
-      System.out.println("STARTING TEST " + testDesignator + "\n");
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+    // CONFIGURE YOUR TESTS HERE
+    public Task3(final String testDesignator) {
+        System.out.println("STARTING TEST " + testDesignator + "\n");
 
-      double zoomFactor = 100; // higher values are closer to the bodies; do not change
+        double zoomFactor = 100; // higher values are closer to the bodies; do not change
+        double timeStep = 0.1; // time step in seconds; do not change
+        double runToTime = 200000; // how long to run in simulation seconds before terminating automatically; collisions terminate at any time; set as needed below
+        int frameDelay = 50; // milliseconds between viewer updates; you may change this depending on how well your machine handles the graphics
 
-      double timeStep = 0.1; // time step in seconds; do not change
-      
-      double runToTime = Double.MAX_VALUE; // how long to run in simulation seconds before terminating automatically; collisions terminate at any time; set as needed below
+        Analyzer analyzer = new Analyzer();
 
-      int frameDelay = 50; // milliseconds between viewer updates; you may change this depending on how well your machine handles the graphics
+        switch (testDesignator) {
+            case "1.A" -> {
+                Body bodyA = new Body("A", -150000, 0, 0, 0, 30000, true);
+                Body bodyB = new Body("B", +150000, 0, 0, 0, 30000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyA, bodyB);
+            }
+            case "1.B" -> {
+                Body bodyA = new Body("A", -150000, 0, 0.5, 0, 30000, true);
+                Body bodyB = new Body("B", +150000, 0, -0.5, 0, 30000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyA, bodyB);
+            }
+            case "1.C" -> {
+                Body bodyA = new Body("A", -150000, 0, -1, 0, 30000, true);
+                Body bodyB = new Body("B", +150000, 0, 1, 0, 30000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyA, bodyB);
+            }
+            case "2.A" -> {
+                Body bodyA = new Body("A", -150000, -130000, 0.5, 0, 30000, true);
+                Body bodyB = new Body("B", +150000, 130000, -0.5, 0, 30000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyA, bodyB);
+            }
+            case "2.B" -> {
+                Body bodyA = new Body("A", -150000, -30000, 2.35, 0, 30000, true);
+                Body bodyB = new Body("B", +150000, 30000, -2.35, 0, 30000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyA, bodyB);
+            }
+            case "3.A" -> {
+                Body bodyEarth = new Body("EARTH", 0, 0, 0, 0, 100000, true);
+                Body bodyRocket = new Body("ROCKET", 0, 55000, 19, 0, 1000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyRocket);
+            }
+            case "3.B" -> {
+                Body bodyEarth = new Body("EARTH", 0, 0, 0, 0, 100000, true);
+                Body bodyRocket = new Body("ROCKET", 0, 55000, 1, 0, 1000, true);
+                int time = 0;
+                while (++time < 32) {
+                    bodyRocket.scheduleThruster(true, 0.1 * time);
+                }
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyRocket);
+            }
+            case "3.C" -> {
+                Body bodyEarth = new Body("EARTH", 0, 0, 0, 0, 100000, true);
+                Body bodyRocket = new Body("ROCKET", 0, 55000, 0, 19, 1000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyRocket);
+            }
+            case "4" -> {
+                Body bodyEarth = new Body("EARTH", 0, 0, 0, 0, 100000, true);
+                Body bodyRocket = new Body("ROCKET", 0, 75000, 23, 0, 1000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyRocket);
+            }
+            case "5.A" -> {
+                Body bodyEarth = new Body("EARTH", 0, 0, 0, 0, 100000, true);
+                Body bodyRocket = new Body("ROCKET", 400000, 100000, -17, 0, 1000, true);
+                bodyRocket.scheduleThruster(false, 20000, 21000, 22000, 23000);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyRocket);
+            }
+            case "5.B" -> {
+                Body bodyEarth = new Body("EARTH", 0, 0, 0, 0, 100000, true);
+                Body bodyRocket = new Body("ROCKET", 400000, 100000, -25, 0, 1000, true);
+                bodyRocket.scheduleThruster(false, 20000, 21000, 22000, 23000);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyRocket);
+            }
+            case "6.A" -> {
+                Body bodyEarth = new Body("EARTH", 0, 0, 0, 0, 100000, true);
+                Body bodyMeteor = new Body("DINO KILLER METEOR", 400000, 60000, -30, 0, 10000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyMeteor);
+            }
+            case "6.B" -> {
+                Body bodyEarth = new Body("EARTH", 0, 0, 0, 0, 100000, true);
+                Body bodyMeteor = new Body("DINO KILLER METEOR", 0, -400000, 0, 10, 10000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyMeteor);
+            }
+            case "7" -> {
+                Body bodyEarth = new Body("EARTH", -50000, 0, 0, 0, 100000, true);
+                Body bodyMoon = new Body("MOON", -50000, 400000, 7, 0, 40000, true);
+                Body bodyRocket = new Body("ROCKET", -50000, 50500, 0, 23, 1000, true);
+                launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyMoon, bodyRocket);
+            }
+            default -> throw new IllegalArgumentException("invalid test [" + testDesignator + "]");
+        }
 
-      Analyzer analyzer = new Analyzer();
+        analyzer.printResults();
+    }
 
-      switch (testDesignator)
-      {
-         case "1.A": // this one is already done
-         {
-             Body bodyA = new Body("A", -150000, 0, 0, 0, 30000, true);
-             Body bodyB = new Body("B", +150000, 0, 0, 0, 30000, true);
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+    // DO NOT TOUCH
+    private void launch(final double zoomFactor,
+                        final int frameDelay,
+                        final double timeStep,
+                        final double runToTime,
+                        final Analyzer analyzer,
+                        final Body... bodies) {
+        System.out.println("STARTING SIMULATOR\n");
 
-             launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyA, bodyB);
+        Simulator simulator = new Simulator(analyzer, timeStep, bodies);
 
-            break;
-         }
+        System.out.println("\nSTARTING VIEWER\n");
 
-         case "1.B":
-         {
-//        	 // add whatever you need
-        	 
-        	 break;
-         }
-         
-         case "1.C":
-         {
-        	 // add whatever you need
-        	 
-        	 break;
-         }
-         
-         case "2.A":
-         {
-         	 // add whatever you need
-        	 
-        	 break;
-         }
-         
-         case "2.B":
-         {
-        	 // add whatever you need
-        	 
-        	 break;
-         }
-         
-         case "3.A":
-         {
-             Body bodyEarth = new Body("EARTH", 0, 0, 0, 0, 100000, false);
-             Body bodyRocket = new Body("ROCKET", 0, 55000, 0, 0, 1000, false);
+        _viewer = new Viewer(simulator, zoomFactor, frameDelay, bodies);
 
-             // add whatever you need
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                _viewer.launch();
+            }
+        });
 
-             launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyRocket);
+        _viewer.refresh();
 
-        	 break;
-         }
-         
-         case "3.B":
-         {
-        	 // add whatever you need
-        	 
-        	 break;
-         }
-         
-         case "3.C":
-         {
-        	 // add whatever you need
-        	 
-        	 break;
-         }
-         
-         case "4":
-         {
-        	 // add whatever you need
-        	 
-        	 break;
-         }
-         
-         case "5.A":
-         {
-             Body bodyEarth = new Body("EARTH", 0, 0, 0, 0, 100000, true);
-             Body bodyRocket = new Body("ROCKET", 400000, 100000, 0, 0, 1000, true);
-             
-        	 // add whatever you need
+        double time = 0;
 
-             launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyRocket);
+        boolean hasCollision = false;
 
-        	 break;
-         }
-         
-         case "5.B":
-         {
-        	 // add whatever you need
-        	 
-        	 break;
-         }
-         
-         case "6.A":
-         {
-        	 // add whatever you need
-        	 
-        	 break;
-         }
-         
-         case "6.B":
-         {
-        	 // add whatever you need
-        	 
-        	 break;
-         }
-         
-         case "7": // grad students only
-         {
-             Body bodyEarth = new Body("EARTH", -50000, 0, 0, 0, 100000, true);
-             Body bodyMoon = new Body("MOON", -50000, 400000, 7, 0, 40000, true);
-             Body bodyRocket = new Body("ROCKET", -50000, 50500, 0, 23, 1000, true);
+        while ((time <= runToTime) && !hasCollision) {
+            simulator.update();
 
-        	 // add whatever you need
-        	 
-             launch(zoomFactor, frameDelay, timeStep, runToTime, analyzer, bodyEarth, bodyMoon, bodyRocket);
+            hasCollision = simulator.hasCollision();
 
-        	 break;
-         }
-            
-         default:
+            time = simulator.getTime();
+        }
 
-            throw new IllegalArgumentException("invalid test [" + testDesignator + "]");
-      }
+        simulator.shutdown();
 
-      analyzer.printResults();
-   }
-
-   // ---------------------------------------------------------------------------------------------------------------------------------------------------------
-   // DO NOT TOUCH
-   private void launch(final double zoomFactor,
-                       final int frameDelay,
-                       final double timeStep,
-                       final double runToTime,
-                       final Analyzer analyzer,
-                       final Body... bodies)
-   {
-      System.out.println("STARTING SIMULATOR\n");
-
-      Simulator simulator = new Simulator(analyzer, timeStep, bodies);
-
-      System.out.println("\nSTARTING VIEWER\n");
-
-      _viewer = new Viewer(simulator, zoomFactor, frameDelay, bodies);
-
-      SwingUtilities.invokeLater(new Runnable()
-      {
-         @Override
-         public void run()
-         {
-            _viewer.launch();
-         }
-      });
-      
-      _viewer.refresh();
-
-      double time = 0;
-
-      boolean hasCollision = false;
-
-      while ((time <= runToTime) && !hasCollision)
-      {
-         simulator.update();
-
-         hasCollision = simulator.hasCollision();
-
-         time = simulator.getTime();
-      }
-
-      simulator.shutdown();
-
-      System.out.println("\nSHUTTING DOWN\n");
-   }
+        System.out.println("\nSHUTTING DOWN\n");
+    }
 }
